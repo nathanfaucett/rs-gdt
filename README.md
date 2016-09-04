b900508a410a8ebe65909177af97f4d8527367ee
nathanfaucett <nathanfaucett@gmail.com>
2016-09-04 12:15:02 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+gdt [![Build Status](https://travis-ci.org/nathanfaucett/rs-gdt.svg?branch=master)](https://travis-ci.org/nathanfaucett/rs-gdt)
+=====
+
+hardware specific global descriptor table
---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+#![no_std]
+
+
+pub mod x86;
+pub mod x86_64;
+
+
+#[cfg(target_arch = "x86")]
+pub use x86::{GdtDescriptor, GdtEntry};
+
+#[cfg(target_arch = "x86_64")]
+pub use x86_64::{GdtDescriptor, GdtEntry};
